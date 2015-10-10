f4dd79edb237be10357e8f0feb95047895cf3a8f
Gunjan Patel <gpatel122@gmail.com>
2015-10-10 16:15:00 +0000

    Create DB with columns for every team

--- db/migrate/20150926190518_create_teams.rb
+++ db/migrate/20150926190518_create_teams.rb
@@ -2,7 +2,20 @@ class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
 
-      t.timestamps
+    	t.string :Pavai
+    	t.string :Gunjan
+    	t.string :Jae
+    	t.string :Kyle
+    	t.string :Steve
+    	t.string :Hirsh
+    	t.string :Pramod
+    	t.string :Iris
+    	t.string :Tesh
+    	t.string :Victor
+    	t.string :Dipal
+    	t.string :Anand
+
+      	t.timestamps
     end
   end
 end
