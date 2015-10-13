c99074ac41dafdca7100d72575d5c683b73247dd
Gunjan Patel <gpatel122@gmail.com>
2015-10-13 04:39:57 +0000

    Draft Results Page Added

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,7 @@
 class TeamsController < ApplicationController
 	def index
 	end
+
+	def draft2015
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Fantasy::Application.routes.draw do
 
   root 'teams#index'
+
+  resources :teams
+  get 'draft2015', :to => 'teams#draft2015'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
